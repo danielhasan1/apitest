0e742a288d30a024b1a4fdf0bef947a5c2c1e39a
danielhasan1 <dh813030@gmail.com>
2019-07-19 04:27:39 +0530

    changed model table pincode = pincode

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def post_location(loc:schemas.Loc, db: Session = Depends(get_db)):
     db_check = crud.get_loc_by_lat_lon(db,latitude = loc.lat, longitude=loc.lon)
     if db_check:
         raise HTTPException(status_code=400, detail="coordinates already exist")
-    row = db.execute("SELECT * FROM pincod WHERE earth_box(ll_to_earth({},{}),2500/1.609) @> ll_to_earth(lat,lon)" .format(loc.lat,loc.lon))
+    row = db.execute("SELECT * FROM pincode WHERE earth_box(ll_to_earth({},{}),2500/1.609) @> ll_to_earth(lat,lon)" .format(loc.lat,loc.lon))
     for i in row:
         st = "coordinates are not accurate too close to existing " + i[0] + ' ' + i[1] + ' ' + str(i[3]) + ' ' + str(i[4])
         raise HTTPException(status_code=400,detail=st)
@@ -143,7 +143,7 @@ def get_near2(lat:float,lon:float,rad:float,db: Session = Depends(get_db)):
     user = session.query().from_statement(
     text("SELECT * FROM pincode")).all()
     '''
-    row = db.execute("SELECT * FROM pincod WHERE earth_box(ll_to_earth({},{}),{}) @> ll_to_earth(lat,lon)" .format(lat,lon,rad))
+    row = db.execute("SELECT * FROM pincode WHERE earth_box(ll_to_earth({},{}),{}) @> ll_to_earth(lat,lon)" .format(lat,lon,rad))
     for i in row:
         l = [{'address':i[1],'loc':i[0],'lat':i[3],'lon':i[4],'city':i[2]} for i in row]
         #print(l)
